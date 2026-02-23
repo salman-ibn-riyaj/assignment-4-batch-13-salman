f998f90a94094402cb00afe6eb107df8144bb544
salman <salmanibnriyaj@gmail.com>
2026-02-23 15:29:05 +0600

    job application tracker section done

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// empty array is here
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total");
+
+let interviewCount = document.getElementById("interviewCount");
+let rejectedCount = document.getElementById("rejectedCount");
+
+let allFilterBtn = document.getElementById("all-filter-btn");
+let interviewFilterBtn = document.getElementById("interview-filter-btn");
+let rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+const allCards = document.getElementById("allCards");
+let mainContainer = document.querySelector("main");
+const filteredSection = document.getElementById("filtered-section");
+let bortomanStatus = "all-filter-btn";
