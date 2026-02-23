1bfb5ea3a52ae964c4aaa095483c837f52f6522c
salman <salmanibnriyaj@gmail.com>
2026-02-23 20:57:59 +0600

    main section started

--- script/script.js
+++ script/script.js
@@ -15,3 +15,33 @@ const allCards = document.getElementById("allCards");
 let mainContainer = document.querySelector("main");
 const filteredSection = document.getElementById("filtered-section");
 let bortomanStatus = "all-filter-btn";
+
+function calculateCount() {
+  total.innerText = allCards.children.length;
+  interviewCount.innerText = interviewList.length;
+  rejectedCount.innerText = rejectedList.length;
+}
+calculateCount();
+
+function toggleStyle(id) {
+  allFilterBtn.classList.remove("btn-primary");
+  interviewFilterBtn.classList.remove("btn-primary");
+  rejectedFilterBtn.classList.remove("btn-primary");
+
+  const selected = document.getElementById(id);
+  bortomanStatus = id;
+  selected.classList.add("btn-primary");
+
+  if (id == "interview-filter-btn") {
+    allCards.classList.add("hidden");
+    filteredSection.classList.remove("hidden");
+    senderInterview();
+  } else if (id == "all-filter-btn") {
+    allCards.classList.remove("hidden");
+    filteredSection.classList.add("hidden");
+  } else if (id == "rejected-filter-btn") {
+    allCards.classList.add("hidden");
+    filteredSection.classList.remove("hidden");
+    senderRejected();
+  }
+}
