2876342f301045d4769e2d71cf65cd170018c882
salman <salmanibnriyaj@gmail.com>
2026-02-24 03:35:45 +0600

    card 2 made

--- script/script.js
+++ script/script.js
@@ -65,3 +65,51 @@ function forSelectedSection(informationCollection) {
       <span class="p-3 border border-gray-300 rounded-full bg-gray-200 cursor-pointer delete-btn"><i class="fa-solid fa-trash-can"></i></span>
     </div>`;
 }
+
+
+mainContainer.addEventListener("click", function (event) {
+ 
+
+
+  if (event.target.classList.contains("interview-btn")) {
+    const parentCard = event.target.parentNode.parentNode;
+    const corpName = parentCard.querySelector(".corpName").innerText;
+    const corpType = parentCard.querySelector(".corpType").innerText;
+    const jobType = parentCard.querySelector(".jobType").innerText;
+    const notes = parentCard.querySelector(".notes").innerText;
+
+
+    parentCard.querySelector(".isApply").innerText = "Interview";
+
+    const cardInfo = {
+      corpName,
+      corpType,
+      jobType,
+      isApply: "Interview",
+      notes,
+    };
+
+ 
+
+
+    rejectedList = rejectedList.filter((item) => item.corpName !== corpName);
+
+   
+    if (!interviewList.find((item) => item.corpName === corpName)) {
+      interviewList.push(cardInfo);
+    }
+
+    calculateCount();
+
+    
+    if (bortomanStatus === "interview-filter-btn") senderInterview();
+    if (bortomanStatus === "rejected-filter-btn") senderRejected();
+  }
+
+
+  else if (event.target.classList.contains("rejected-btn")) {
+    const parentCard = event.target.parentNode.parentNode;
+    const corpName = parentCard.querySelector(".corpName").innerText;
+    const corpType = parentCard.querySelector(".corpType").innerText;
+    const jobType = parentCard.querySelector(".jobType").innerText;
+    const notes = parentCard.querySelector(".notes").innerText;
